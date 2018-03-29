fda72c0b74d84b8e3f31ce8aff3bec76a8a99544
Dmytro Panasiuk <dmitriy.panasyuk@gmail.com>
2018-03-29 23:23:06 +0300

    Create main window with grid

--- src/main.py
+++ src/main.py
@@ -1,26 +1,35 @@
 import tkinter as tk
 
+WINDOW_HEIGHT = 500
+WINDOW_WIDTH = 500
+GRID_LEN = 4
+BACKGROUND_COLOR = '#92877d'
+BACKGROUND_COLOR_EMPTY = "#9e948a"
+FONT = ("Verdana", 40, "bold")
+
 
 class Application(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
+    def __init__(self):
+        super().__init__()
         self.pack()
-        self.create_widgets()
+        self.master.title('2048')
+        self.init()
+        self.mainloop()
 
-    def create_widgets(self):
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["command"] = self.say_hi
-        self.hi_there.pack(side="top")
+    def init(self):
+        background = tk.Frame(master=self, width=WINDOW_WIDTH, height=WINDOW_HEIGHT, bg=BACKGROUND_COLOR)
+        # background.pack()
+        background.grid()
+        for i in range(GRID_LEN):
+            grid_row = []
+            for j in range(GRID_LEN):
+                cell = tk.Frame(background, bg=BACKGROUND_COLOR_EMPTY, width=WINDOW_WIDTH/GRID_LEN, height=WINDOW_HEIGHT/GRID_LEN)
+                # cell.pack()
+                cell.grid(row=i, column=j, padx=1, pady=1)
+                t = tk.Label(master=cell, text="", bg=BACKGROUND_COLOR_EMPTY,
+                             justify=tk.CENTER, font=FONT, width=4, height=2)
+                t.grid()
 
-        self.quit = tk.Button(self, text="QUIT", fg="red",
-                              command=root.destroy)
-        self.quit.pack(side="bottom")
 
-    def say_hi(self):
-        print("hi there, everyone!")
-
-
-root = tk.Tk()
-app = Application(master=root)
-app.mainloop()
+if __name__ == '__main__':
+    app = Application()
